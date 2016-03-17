36f7cd0a63430fbb060fee68930654d3ad84b889
Questlog <luaworfox@gmail.com>
2016-03-17 19:26:01 +0100

    Updated aspects

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
-from distutils.core import setup
-import py2exe
+import sys
+from cx_Freeze import setup, Executable
 
-setup( windows=[{"script": "Aspector.py"}] )
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(  name = "Aspector",
+        version = "0.1",
+        description = "Aspector!",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("Aspector.py", base=base)])
